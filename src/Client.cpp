0c561a64942731fb081237e9d60a254ec7f438e4
FabioAzevedoGomes <fabioazevedogomes@hotmail.com>
2020-10-19 10:12:54 -0300

    Time in client interface

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,19 +2,22 @@
 
 std::atomic<bool> Client::stop_issued;
 int Client::server_socket;
+
 pthread_t Client::input_handler_thread;
 pthread_t Client::keep_alive_thread;
+pthread_t Client::ui_update_thread;
+
 std::string Client::username;
 RW_Monitor Client::socket_monitor;
 
 Client::Client(std::string username, std::string groupname, std::string server_ip, std::string server_port)
 {
     // Validate username
-    if ( !std::regex_match(username,std::regex(NAME_REGEX)) )
+    if (!std::regex_match(username, std::regex(NAME_REGEX)))
         throw std::invalid_argument("Invalid username format");
 
     // Valide groupname
-    if ( !std::regex_match(groupname, std::regex(NAME_REGEX)))
+    if (!std::regex_match(groupname, std::regex(NAME_REGEX)))
         throw std::invalid_argument("Invalid group name format");
 
     // Validate IP
@@ -28,7 +31,7 @@ Client::Client(std::string username, std::string groupname, std::string server_i
     // Initialize values
     this->username = username;
     this->groupname = groupname;
-    this->server_ip = !server_ip.compare("localhost")? "127.0.0.1" : server_ip;
+    this->server_ip = !server_ip.compare("localhost") ? "127.0.0.1" : server_ip;
     this->server_port = stoi(server_port);
     this->server_socket = -1;
 
@@ -36,7 +39,7 @@ Client::Client(std::string username, std::string groupname, std::string server_i
     stop_issued = false;
 
     // Initialize client interface
-    ClientInterface::init(groupname);   
+    ClientInterface::init(groupname, username);
 };
 
 Client::~Client()
@@ -51,15 +54,15 @@ Client::~Client()
 
 void Client::setupConnection()
 {
-    message_record* login_record; // Record for sending login packet
+    message_record *login_record; // Record for sending login packet
 
     // Create socket
-    if ( (server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         throw std::runtime_error(appendErrorMessage("Error during socket creation"));
 
     // Fill server socket address
     server_address.sin_family = AF_INET;
-    server_address.sin_port   = htons(server_port);
+    server_address.sin_port = htons(server_port);
     server_address.sin_addr.s_addr = inet_addr(server_ip.c_str());
 
     // Try to connect to remote server
@@ -70,7 +73,7 @@ void Client::setupConnection()
     login_record = CommunicationUtils::composeMessage(username, std::string(groupname), LOGIN_MESSAGE);
 
     // Sends the command packet to the server
-    CommunicationUtils::sendPacket(server_socket, PAK_COMMAND, (char*)login_record, sizeof(*login_record) + login_record->length); 
+    CommunicationUtils::sendPacket(server_socket, PAK_COMMAND, (char *)login_record, sizeof(*login_record) + login_record->length);
 
     // Free login record
     free(login_record);
@@ -80,99 +83,101 @@ void Client::setupConnection()
 
     // Start keep-alive thread
     pthread_create(&keep_alive_thread, NULL, keepAlive, NULL);
+
+    // Start UI updater thread
+    pthread_create(&ui_update_thread, NULL, ClientInterface::updateUI, NULL);
 };
 
 void Client::getMessages()
 {
-    int read_bytes = -1;                // Number of bytes read from the header
-    char server_message[PACKET_MAX];    // Buffer for message sent from server
-    message_record* received_message;   // Pointer to a message record, used to decode received packet payload
-    packet* received_packet;
+    int read_bytes = -1;              // Number of bytes read from the header
+    char server_message[PACKET_MAX];  // Buffer for message sent from server
+    message_record *received_message; // Pointer to a message record, used to decode received packet payload
+    packet *received_packet;
 
     char message_time[9];     // Timestamp of the message
     std::string chat_message; // Final composed chat message string, printed to the interface
     std::string username;     // Name of the user who sent the message
 
     // Clear buffer to receive new packets
-    bzero((void*)server_message, PACKET_MAX);
+    bzero((void *)server_message, PACKET_MAX);
 
     // Wait for messages from the server
-    while(!stop_issued && (read_bytes = CommunicationUtils::receivePacket(server_socket, server_message, PACKET_MAX)) > 0)
+    while (!stop_issued && (read_bytes = CommunicationUtils::receivePacket(server_socket, server_message, PACKET_MAX)) > 0)
     {
         // Decode message into packet format
-        received_packet = (packet*)server_message;
+        received_packet = (packet *)server_message;
 
         // Try to read the rest of the payload from the socket stream
-        switch(received_packet->type)
+        switch (received_packet->type)
         {
-            case PAK_DATA: // Data packet (messages)
+        case PAK_DATA: // Data packet (messages)
 
-                // Decode payload into a message record
-                received_message = (message_record*)received_packet->_payload;
+            // Decode payload into a message record
+            received_message = (message_record *)received_packet->_payload;
 
-                // If message was sent by this user, change display name to "You"
-                if (strcmp(received_message->username, this->username.c_str()) == 0)
-                {
-                    sprintf(received_message->username,"%s","You");
-                }
-                // If not, add brackets to display name: [username]
-                else
-                {
-                    username = std::string("[") + received_message->username + "]";
-                    sprintf(received_message->username,"%s",username.c_str());
-                }          
+            // If message was sent by this user, change display name to "You"
+            if (strcmp(received_message->username, this->username.c_str()) == 0)
+            {
+                sprintf(received_message->username, "%s", "You");
+            }
+            // If not, add brackets to display name: [username]
+            else
+            {
+                username = std::string("[") + received_message->username + "]";
+                sprintf(received_message->username, "%s", username.c_str());
+            }
 
-                // Get time into a readable format
-                strftime(message_time, sizeof(message_time), "%H:%M:%S", std::localtime((time_t*)&received_message->timestamp));
+            // Get time into a readable format
+            strftime(message_time, sizeof(message_time), "%H:%M:%S", std::localtime((time_t *)&received_message->timestamp));
 
-                if (received_message->type == SERVER_MESSAGE)
-                {
-                    chat_message = message_time + std::string(" ") + received_message->_message;
-                }
-                else if (received_message->type == USER_MESSAGE)
-                {
-                    chat_message = message_time + std::string(" ") + received_message->username + ": " + received_message->_message;
-                }
+            if (received_message->type == SERVER_MESSAGE)
+            {
+                chat_message = message_time + std::string(" ") + received_message->_message;
+            }
+            else if (received_message->type == USER_MESSAGE)
+            {
+                chat_message = message_time + std::string(" ") + received_message->username + ": " + received_message->_message;
+            }
 
-                // Display message
-                ClientInterface::printMessage(chat_message);
-                break;
+            // Display message
+            ClientInterface::printMessage(chat_message);
+            break;
 
-            case PAK_SERVER_MESSAGE: // Server broadcast message for clients login/logout 
-                
-                // Decode payload into a message record
-                received_message = (message_record*)received_packet->_payload;
+        case PAK_SERVER_MESSAGE: // Server broadcast message for clients login/logout
 
-                // Get time into a readable format
-                strftime(message_time, sizeof(message_time), "%H:%M:%S", std::localtime((time_t*)&received_message->timestamp));
+            // Decode payload into a message record
+            received_message = (message_record *)received_packet->_payload;
 
-                // Display message
-                chat_message = message_time + std::string(" ") + std::string(received_message->_message);
-                ClientInterface::printMessage(chat_message);
+            // Get time into a readable format
+            strftime(message_time, sizeof(message_time), "%H:%M:%S", std::localtime((time_t *)&received_message->timestamp));
 
-                break;
+            // Display message
+            chat_message = message_time + std::string(" ") + std::string(received_message->_message);
+            ClientInterface::printMessage(chat_message);
 
-            case PAK_COMMAND: // Command packet (disconnect)
+            break;
 
-                // Decode payload into a message record
-                received_message = (message_record*)received_packet->_payload;
+        case PAK_COMMAND: // Command packet (disconnect)
 
-                // Show message to user
-                ClientInterface::printMessage(received_message->_message);
+            // Decode payload into a message record
+            received_message = (message_record *)received_packet->_payload;
 
-                // Stop the client application
-                stop_issued = true;
-                read_bytes = 0;
-                break;
+            // Show message to user
+            ClientInterface::printMessage(received_message->_message);
 
-            default: // Unknown packet
-                ClientInterface::printMessage("Received unkown packet from server");
-                break;
+            // Stop the client application
+            stop_issued = true;
+            read_bytes = 0;
+            break;
+
+        default: // Unknown packet
+            ClientInterface::printMessage("Received unkown packet from server");
+            break;
         }
 
         // Clear buffer to receive new packets
-        bzero((void*)server_message, PACKET_MAX);
-
+        bzero((void *)server_message, PACKET_MAX);
     }
     // If server closes connection
     if (read_bytes == 0)
@@ -184,22 +189,26 @@ void Client::getMessages()
     stop_issued = true;
 
     // Wait for input handler thread to finish
-    pthread_join(Client::input_handler_thread,NULL);
+    pthread_join(Client::input_handler_thread, NULL);
 
     // Wake up the keep-alive thread from it's sleep
     pthread_cancel(Client::keep_alive_thread);
     pthread_join(Client::keep_alive_thread, NULL);
+
+    // Finish the UI updater thread
+    pthread_cancel(Client::ui_update_thread);
+    pthread_join(Client::ui_update_thread, NULL);
 };
 
-void *Client::handleUserInput(void* arg)
+void *Client::handleUserInput(void *arg)
 {
-    message_record* message;
+    message_record *message;
 
     // Get user messages to be sent until Ctrl D is pressed
     char user_message[MESSAGE_MAX + 1];
     do
     {
-        if (wgetnstr(ClientInterface::inptscr,user_message, MESSAGE_MAX) == ERR)
+        if (wgetnstr(ClientInterface::inptscr, user_message, MESSAGE_MAX) == ERR)
             stop_issued = true;
         if (!stop_issued)
         {
@@ -208,7 +217,7 @@ void *Client::handleUserInput(void* arg)
                 // Reset input area below the screen
                 ClientInterface::resetInput();
 
-                if (strlen(user_message) > 0) 
+                if (strlen(user_message) > 0)
                 {
                     // Compose message
                     message = CommunicationUtils::composeMessage(username, std::string(user_message), USER_MESSAGE);
@@ -217,7 +226,7 @@ void *Client::handleUserInput(void* arg)
                     socket_monitor.requestWrite();
 
                     // Send message to server
-                    CommunicationUtils::sendPacket(server_socket, PAK_DATA, (char*)message, sizeof(*message) + message->length);
+                    CommunicationUtils::sendPacket(server_socket, PAK_DATA, (char *)message, sizeof(*message) + message->length);
 
                     // Release write rights
                     socket_monitor.releaseWrite();
@@ -225,17 +234,15 @@ void *Client::handleUserInput(void* arg)
                     // Free composed message
                     free(message);
                 }
-
             }
-            catch(const std::runtime_error& e)
+            catch (const std::runtime_error &e)
             {
                 std::cerr << e.what() << std::endl;
             }
         }
 
-        bzero((void*)user_message, MESSAGE_MAX + 1);
-    }
-    while(!stop_issued);
+        bzero((void *)user_message, MESSAGE_MAX + 1);
+    } while (!stop_issued);
 
     // Signal server-litening thread to stop
     stop_issued = true;
@@ -247,21 +254,21 @@ void *Client::handleUserInput(void* arg)
     pthread_exit(NULL);
 };
 
-void *Client::keepAlive(void* arg)
+void *Client::keepAlive(void *arg)
 {
     // Useless message that will be sent to server to keep connection going
     char keep_alive = '\0';
 
-    while(!stop_issued)
+    while (!stop_issued)
     {
         // Sleep for SLEEP_TIME seconds between attempting to send messages to the server
         sleep(SLEEP_TIME);
-    
+
         if (!stop_issued)
         {
             // Request write rights
             socket_monitor.requestWrite();
-        
+
             // Send message
             CommunicationUtils::sendPacket(Client::server_socket, PAK_KEEP_ALIVE, &keep_alive, sizeof(keep_alive));
 
